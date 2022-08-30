a24623c0a5ae5b768d4156eda93e00a6fa103cbd
AndrewBurUU <andriv80@gmail.com>
2022-08-30 09:05:48 +0800

    8. Реализовать собственную структуру данных - HashMap [#1008 #337443]

--- src/test/java/ru/job4j/map/SimpleMapTest.java
+++ src/test/java/ru/job4j/map/SimpleMapTest.java
@@ -123,7 +123,6 @@ class SimpleMapTest {
         assertThat(map).hasSize(4);
     }
 
-
     @Test
     void whenCheckPutZeroAndNull() {
         SimpleMap<Integer, String> map = new SimpleMap<>();
