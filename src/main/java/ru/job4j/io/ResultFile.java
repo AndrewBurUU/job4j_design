80a93bd66950f5d547ca0eb47264f090f50c21ae
AndrewBurUU <andriv80@gmail.com>
2022-09-02 20:08:43 +0800

    0.1. FileOutputStream. [#252488 #338834]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -6,22 +6,11 @@ public class ResultFile {
 
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            out.write("1*2=2".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("1*3=3".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("1*4=4".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("1*5=5".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("1*6=6".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("1*7=7".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("1*8=8".getBytes());
-            out.write(System.lineSeparator().getBytes());
-            out.write("1*9=9".getBytes());
-            out.write(System.lineSeparator().getBytes());
+            for (int i = 1; i < 9; i++) {
+                String str = 1 + "*" + (i + 1) + "=" + (i + 1);
+                out.write(str.getBytes());
+                out.write(System.lineSeparator().getBytes());
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
