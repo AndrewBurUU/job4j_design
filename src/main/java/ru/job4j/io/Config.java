0799e273b5703ae1f1a67ec58bf570e9e621278b
AndrewBurUU <andriv80@gmail.com>
2022-09-02 15:58:58 +0800

    1. Читаем файл конфигурации [#858]

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -13,6 +13,19 @@ public class Config {
     }
 
     public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            String str;
+            while ((str = read.readLine()) != null) {
+                if (!str.isEmpty() && !str.startsWith("#") && str.contains("=")) {
+                    String[] pair = str.split("[@+=*%]");
+                    values.put(pair[0], pair[1]);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public String value(String key) {
@@ -23,7 +36,7 @@ public class Config {
     public String toString() {
         StringJoiner out = new StringJoiner(System.lineSeparator());
         try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
-            read.lines().forEach(out::add);
+            read.lines().forEach(newElement -> out.add(newElement));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -31,6 +44,15 @@ public class Config {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Config("app.properties"));
+
+        //System.out.println(new Config("app.properties"));
+        String str = "111123=abcddddd";
+        String[] pair = str.split("[@+=*%]");
+        if (pair.length == 1)
+            return;
+        String delim = str.substring(pair[0].length(), str.length() - pair[1].length());
+        System.out.println(pair[0]); // 123
+        System.out.println(delim);   // &
+        System.out.println(pair[1]); // abcd
     }
 }
