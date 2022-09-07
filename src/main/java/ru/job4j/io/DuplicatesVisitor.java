20643d88642f6582c178d3ff9b3b8506a02e98e8
AndrewBurUU <andriv80@gmail.com>
2022-09-07 12:41:44 +0800

    4.2. Поиск дубликатов [#315066 #340520]

--- src/main/java/ru/job4j/io/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/DuplicatesVisitor.java
@@ -9,7 +9,7 @@ import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-    private Map<Path, FileProperty> filesMap;
+    private Map<FileProperty, Path> filesMap;
     private Map<FileProperty, ArrayList<Path>> fileDuplicatesMap;
 
     public DuplicatesVisitor() {
@@ -24,22 +24,18 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty newFile = new FileProperty(file.getFileName().toString(), attrs.size());
-        if (filesMap.containsValue(newFile)) {
-            for (Path mapPath : filesMap.keySet()) {
-                FileProperty mapFile = filesMap.get(mapPath);
-                if (newFile.equals(mapFile)) {
-                    ArrayList<Path> tmpArray = new ArrayList<>();
-                    if (fileDuplicatesMap.containsKey(mapFile)) {
-                        tmpArray = fileDuplicatesMap.get(mapFile);
-                    } else {
-                        tmpArray.add(mapPath);
-                    }
-                    tmpArray.add(file);
-                    fileDuplicatesMap.put(mapFile, tmpArray);
-                }
+        if (filesMap.containsKey(newFile)) {
+            Path mapPath = filesMap.get(newFile);
+            ArrayList<Path> tmpArray = new ArrayList<>();
+            if (fileDuplicatesMap.containsKey(newFile)) {
+                tmpArray = fileDuplicatesMap.get(newFile);
+            } else {
+                tmpArray.add(mapPath);
             }
+            tmpArray.add(file);
+            fileDuplicatesMap.put(newFile, tmpArray);
         } else {
-            filesMap.put(file, newFile);
+            filesMap.put(newFile, file);
         }
         return super.visitFile(file, attrs);
     }
