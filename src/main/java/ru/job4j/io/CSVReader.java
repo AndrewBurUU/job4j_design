40586b2c5eb23dbdd4a8c266b918d7118301cc51
AndrewBurUU <andriv80@gmail.com>
2022-09-14 15:28:45 +0800

    7. Scanner [#504791 #342778]

---
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -0,0 +1,93 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.*;
+
+public class CSVReader {
+
+    public static void argumentsValidate(ArgsName arguments) {
+        File fileIn = new File(arguments.get("path"));
+        if (!fileIn.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", fileIn.getAbsoluteFile()));
+        }
+        if (arguments.get("delimiter").length() != 1) {
+            throw new IllegalArgumentException("Delimiter length must be equal to one");
+        }
+        if (!"stdout".equals(arguments.get("out"))) {
+            File fileOut = new File(arguments.get("out"));
+            if (fileIn.equals(fileOut)) {
+                throw new IllegalArgumentException("FileIn and FileOut must be different");
+            }
+        }
+        String filter = arguments.get("filter");
+        if (filter.length() == 0) {
+            throw new IllegalArgumentException("Empty filter fields");
+        } else {
+            if (filter.indexOf(",") == -1) {
+                throw new IllegalArgumentException(String.format("Illegal filter fields delimiter: %s", filter));
+            }
+        }
+    }
+
+    public static void handle(ArgsName argsName) throws Exception {
+        File sourceFile = new File(argsName.get("path"));
+        File targetFile = new File(argsName.get("out"));
+        if ("stdout".equals(targetFile.toString())) {
+            targetFile = sourceFile;
+        }
+        String[] filterNames = argsName.get("filter").split(",");
+        int countLines = 0;
+        List<Integer> resIndex = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(sourceFile));
+             PrintWriter out = new PrintWriter(new FileOutputStream(targetFile))) {
+            String sourceString = "";
+            while ((sourceString = in.readLine()) != null) {
+                var scanner = new Scanner(new ByteArrayInputStream(sourceString.getBytes()))
+                        .useDelimiter(argsName.get("delimiter"));
+                List<String> fieldsList = new ArrayList<>();
+                String res = "";
+                while (scanner.hasNext()) {
+                    fieldsList.add(scanner.next());
+                }
+                if (countLines == 0) {
+                    for (int i = 0; i < filterNames.length; i++) {
+                        int index = fieldsList.indexOf(filterNames[i]);
+                        if (index >= 0) {
+                            resIndex.add(index);
+                        }
+                    }
+                }
+                for (int i = 0; i < resIndex.size(); i++) {
+                    int index = resIndex.get(i);
+                    if (res.length() == 0) {
+                        res = fieldsList.get(index);
+                    } else {
+                        res = res + ";" + fieldsList.get(index);
+                    }
+                }
+                out.println(res);
+                countLines++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readLog(File file) {
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            in.lines().forEach(s -> System.out.println(s));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 4) {
+            throw new IllegalArgumentException("Incorrect parameter numbers.");
+        }
+        ArgsName jvm = ArgsName.of(args);
+        argumentsValidate(jvm);
+        handle(jvm);
+        readLog(new File(jvm.get("out")));
+    }
+}
