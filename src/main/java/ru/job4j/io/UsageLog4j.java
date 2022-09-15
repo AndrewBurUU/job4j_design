f55fbfdbf62bf45eddc44d945dd657a4b3fe95a8
AndrewBurUU <andriv80@gmail.com>
2022-09-15 11:18:50 +0800

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,17 +8,10 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        byte typeByte = 10;
-        short typeShort = 100;
-        int typeInt = 1000;
-        long typeLong = 1000;
-        float typeFloat = 1000;
-        double typeDouble = 1000;
-        char typeChar = 'z';
-        boolean typeBoolean = true;
-        LOG.debug("Byte: {}, short: {}, int: {}, long: {}, "
-                        + "float: {}, double: {}, char: {}, boolean: {}",
-                typeByte, typeShort, typeInt, typeLong,
-                typeFloat, typeDouble, typeChar, typeBoolean);
+        try {
+            throw new Exception("Not supported code");
+        } catch (Exception e) {
+            LOG.error("User {}. Error:", "Petr", e);
+        }
     }
 }
