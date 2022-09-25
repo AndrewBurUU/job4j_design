50cea080ed39ede79d0b0b553cb89bbeb8e443c0
AndrewBurUU <andriv80@gmail.com>
2022-09-26 07:25:39 +0800

    0. Select [#6861]

--- data/tmp/DB_Items/fauna.sql
+++ data/tmp/DB_Items/fauna.sql
@@ -1,3 +1,14 @@
+create table fauna (
+    id serial primary key,
+    name text,
+    avg_age int,
+    discovery_date date
+);
+
+insert into fauna(name, avg_age, discovery_date) values('fish', 11000, null);
+insert into fauna(name, avg_age, discovery_date) values('monkey', 25000, '1949-01-01');
+insert into fauna(name, avg_age, discovery_date) values('animal1', 9000, '2000-01-01');
+
 select *
 from fauna
 where name like '%fish%';
