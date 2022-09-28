8a160e66715a8ee208a41d42d79c8f952b5ecff4
AndrewBurUU <andriv80@gmail.com>
2022-09-28 12:41:02 +0800

    3. Фильтры [#1732 #346528]

--- data/tmp/DB_Items/productshop.sql
+++ data/tmp/DB_Items/productshop.sql
@@ -17,12 +17,31 @@ insert into product(name, type_id, expired_date, price) values('сыр1', 1, '01
 insert into product(name, type_id, expired_date, price) values('сыр2', 1, '01.09.2022', 1500), ('масло1', 2, '05.10.2022', 150);
 insert into product(name, type_id, expired_date, price) values('молоко1', 3, '01.09.2022', 90), ('масло2', 2, '05.10.2022', 200);
 
-select * from product where type_id = 1;
-select * from product where name like '%мороженое%'
-select * from product where expired_date < '26.09.2022'
-select max(price) from product
-select type_id, count(id) from product group by type_id
-select name from product where type_id = 1 OR type_id = 3
-select type_id, count(id) from product group by type_id having count(id) < 10
-select p.name, t.name from product p join type t on p.type_id=t.id
+select p.name, t.name, p.price, p.expired_date
+from type t join product p on t.id = p.type_id
+where t.name = 'СЫР'
 
+select * from product where name like '%мороженое%'
+select * from product where expired_date < current_date
+
+select p1.name, p1.price
+from product p1 
+join 
+(select max(price) as pprice
+ from product p2) p3
+on p1.price = p3.pprice
+
+select t.name, count(p.id)
+from type t join product p on t.id=p.type_id
+group by t.name
+
+select p.name
+from type t join product p on t.id=p.type_id
+where t.name = 'СЫР' OR t.name = 'МОЛОКО'
+
+select t.name, count(p.id)
+from type t join product p on t.id=p.type_id
+group by t.name
+having count(p.id) < 10
+
+select p.name, t.name from product p join type t on p.type_id=t.id
