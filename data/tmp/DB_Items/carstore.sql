bab0aee2184be387c7dba0c1e48ff66057e2005e
AndrewBurUU <andriv80@gmail.com>
2022-09-29 09:20:31 +0800

    Хранилище машин [#1733 #346546]

--- data/tmp/DB_Items/carstore.sql
+++ data/tmp/DB_Items/carstore.sql
@@ -26,21 +26,31 @@ insert into carengines(name) values('бензин'), ('дизель');
 insert into cartransmissions(name) values('механика'), ('автомат'), ('вариатор');
 
 insert into cars(name) values('car0');
-insert into cars(name, body_id, engine_id, transmission_id) values('car1', 1, 1, 2);
-insert into cars(name, body_id, engine_id, transmission_id) values('car2', 1, 1, 1);
-insert into cars(name, body_id, engine_id, transmission_id) values('car3', 2, 1, 1);
 
-select c.id, c.name, cb.name, c.engine_id, c.transmission_id
-from cars c join carbodies cb on c.body_id = cb.id
+insert into cars(name, body_id, engine_id, transmission_id) 
+values('car1', 1, 1, 2);
+
+insert into cars(name, body_id, engine_id, transmission_id) 
+values('car2', 1, 1, 1);
+
+insert into cars(name, body_id, engine_id, transmission_id) 
+values('car3', 2, 1, 1);
+
+select c.id, c.name car_name, 
+cb.name body_name, ce.name engine_name, cm.name transmission_name
+from cars c
+left join carbodies cb on c.body_id = cb.id
+left join carengines ce on c.engine_id = ce.id
+left join cartransmissions cm on c.transmission_id = cm.id;
 
 select cb.name
 from carbodies cb left join cars c on cb.id = c.body_id 
-where body_id is null
+where body_id is null;
 
 select ce.name
 from carengines ce left join cars c on ce.id = c.engine_id 
-where engine_id is null
+where engine_id is null;
 
 select ct.name
 from cartransmissions ct left join cars c on ct.id = c.transmission_id 
-where body_id is null
+where body_id is null;
