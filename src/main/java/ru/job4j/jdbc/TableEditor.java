7902ef02369bbcc644170e466865de38e5276011
AndrewBurUU <andriv80@gmail.com>
2022-10-04 17:18:04 +0800

    0.1. Statement [#379306 #348172]

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -23,62 +23,45 @@ public class TableEditor implements AutoCloseable {
         connection = DriverManager.getConnection(url, login, password);
     }
 
-    public void createTable(String tableName) {
+    private void initStatement(String sql) {
         try (Statement statement = connection.createStatement()) {
-             String sql = String.format(
-                     "create table if not exists %s();", tableName);
-             statement.execute(sql);
+            statement.execute(sql);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void createTable(String tableName) {
+        initStatement(String.format(
+                "create table if not exists %s();", tableName));
     }
 
     public void dropTable(String tableName) {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "drop table if exists %s;", tableName);
-            statement.execute(sql);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        initStatement(String.format(
+                "drop table if exists %s;", tableName));
     }
 
     public void addColumn(String tableName, String columnName, String type) {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "alter table %s add column %s %s;",
-                    tableName,
-                    columnName,
-                    type);
-            statement.execute(sql);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        initStatement(String.format(
+                "alter table %s add column %s %s;",
+                tableName,
+                columnName,
+                type));
     }
 
     public void dropColumn(String tableName, String columnName) {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "alter table %s drop column %s;",
-                    tableName,
-                    columnName);
-            statement.execute(sql);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        initStatement(String.format(
+                "alter table %s drop column %s;",
+                tableName,
+                columnName));
     }
 
     public void renameColumn(String tableName, String columnName, String newColumnName) {
-        try (Statement statement = connection.createStatement()) {
-            String sql = String.format(
-                    "alter table %s rename column %s to %s;",
-                    tableName,
-                    columnName,
-                    newColumnName);
-            statement.execute(sql);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        initStatement(String.format(
+                "alter table %s rename column %s to %s;",
+                tableName,
+                columnName,
+                newColumnName));
     }
 
     public static String getTableScheme(Connection connection, String tableName) throws Exception {
