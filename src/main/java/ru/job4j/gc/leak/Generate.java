3dea63b1ccbbbb911a2bcaf22d54c95c6f146bb0
AndrewBurUU <andriv80@gmail.com>
2022-10-20 15:49:06 +0800

    2. Найти утечку памяти. [#504882 #353132]

--- src/main/java/ru/job4j/gc/leak/Generate.java
+++ src/main/java/ru/job4j/gc/leak/Generate.java
@@ -1,8 +1,10 @@
 package ru.job4j.gc.leak;
 
 import java.io.*;
+import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.*;
 
 public interface Generate {
 
@@ -10,8 +12,8 @@ public interface Generate {
 
     default List<String> read(String path) throws IOException {
         List<String> text = new ArrayList<>();
-        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
-            in.lines().forEach(text::add);
+        try (Stream<String> lines = Files.lines(Paths.get(path))) {
+            lines.forEach(text::add);
         }
         return text;
     }
