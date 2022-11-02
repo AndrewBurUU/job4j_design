cf5d27febe3c72f1466406a8fbcd754c20479b8d
AndrewBurUU <andriv80@gmail.com>
2022-11-02 17:52:00 +0800

    1. Отчеты. [#850 #355697]

--- src/main/java/ru/job4j/ood/srp/report/ReportBookKeeping.java
+++ src/main/java/ru/job4j/ood/srp/report/ReportBookKeeping.java
@@ -13,17 +13,14 @@ public class ReportBookKeeping implements Report {
 
     private final Store store;
     private final DateTimeParser<Calendar> dateTimeParser;
-    private final Currency currencyFrom;
-    private final Currency currencyTo;
+    private final CurrencyConverter converter;
 
     public ReportBookKeeping(Store store,
                              DateTimeParser<Calendar> dateTimeParser,
-                             Currency currencyFrom,
-                             Currency currencyTo) {
+                             CurrencyConverter converter) {
         this.store = store;
         this.dateTimeParser = dateTimeParser;
-        this.currencyFrom = currencyFrom;
-        this.currencyTo = currencyTo;
+        this.converter = converter;
     }
 
     @Override
@@ -32,8 +29,7 @@ public class ReportBookKeeping implements Report {
         text.append("Name; Hired; Fired; Salary;")
                 .append(System.lineSeparator());
         for (Employee employee : store.findBy(filter)) {
-            InMemoryCurrencyConverter inMemoryCurrencyConverter = new InMemoryCurrencyConverter();
-            double salary = inMemoryCurrencyConverter.convert(currencyFrom, employee.getSalary(), currencyTo);
+            double salary = converter.convert(Currency.USD, employee.getSalary(), Currency.RUB);
             text.append(employee.getName()).append(" ")
                     .append(dateTimeParser.parse(employee.getHired())).append(" ")
                     .append(dateTimeParser.parse(employee.getFired())).append(" ")
@@ -41,26 +37,5 @@ public class ReportBookKeeping implements Report {
                     .append(System.lineSeparator());
         }
         return text.toString();
-    }
-
-    public static void main(String[] args) {
-        MemStore store = new MemStore();
-        Employee worker = new Employee("Ivan",
-                new GregorianCalendar(2020, Calendar.OCTOBER, 01),
-                new GregorianCalendar(2021, Calendar.OCTOBER, 01),
-                100);
-        store.add(worker);
-        worker = new Employee("Piter",
-                new GregorianCalendar(2020, Calendar.NOVEMBER, 01),
-                new GregorianCalendar(2021, Calendar.NOVEMBER, 01),
-                150);
-        store.add(worker);
-        DateTimeParser<Calendar> parser = new ReportDateTimeParser();
-        Report engine = new ReportBookKeeping(store,
-                parser,
-                Currency.USD,
-                Currency.RUB);
-        String res = engine.generate(em -> true);
-        System.out.println(res);
     }
 }
--- src/test/java/ru/job4j/ood/srp/report/ReportEngineTest.java
+++ src/test/java/ru/job4j/ood/srp/report/ReportEngineTest.java
@@ -3,6 +3,7 @@ package ru.job4j.ood.srp.report;
 import com.google.gson.*;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.io.*;
+import ru.job4j.ood.srp.currency.*;
 import ru.job4j.ood.srp.currency.Currency;
 import ru.job4j.ood.srp.formatter.DateTimeParser;
 import ru.job4j.ood.srp.formatter.ReportDateTimeParser;
@@ -65,10 +66,9 @@ class ReportEngineTest {
                 150);
         store.add(worker);
         DateTimeParser<Calendar> parser = new ReportDateTimeParser();
+        InMemoryCurrencyConverter inMemoryCurrencyConverter = new InMemoryCurrencyConverter();
         Report engine = new ReportBookKeeping(store,
-                parser,
-                Currency.USD,
-                Currency.RUB);
+                parser,inMemoryCurrencyConverter);
         String res = engine.generate(em -> true);
         assertThat(expected.toString()).isEqualTo(res);
     }
