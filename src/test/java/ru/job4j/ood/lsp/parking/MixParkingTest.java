17715cb2ff3654bb5c6e30893c9a002ae90fb70d
AndrewBurUU <andriv80@gmail.com>
2022-11-15 17:47:14 +0800

    2. Парковка машин [#853 #359259]

--- src/test/java/ru/job4j/ood/lsp/parking/MixParkingTest.java
+++ src/test/java/ru/job4j/ood/lsp/parking/MixParkingTest.java
@@ -11,21 +11,56 @@ import static org.assertj.core.api.Assertions.*;
 class MixParkingTest {
 
     @Test
+    void whenOnlyCars() {
+        Transport car1 = new Car("Lada", "в777аа");
+        Transport car2 = new Car("Mercedes", "к520аа");
+        Transport truck = new Truck("Kamaz", "а111аа", 6);
+        MixParking mixParking = new MixParking(5, 0);
+        assertThat(mixParking.add(car1)).isTrue();
+        assertThat(mixParking.add(car2)).isTrue();
+        assertThat(mixParking.add(truck)).isFalse();
+    }
+
+    @Test
     void whenTwoCarsOneTruck() {
         Transport car1 = new Car("Lada", "в777аа");
         Transport car2 = new Car("Mercedes", "к520аа");
         Transport truck = new Truck("Kamaz", "а111аа", 1);
-        List<Transport> expectedCarList = List.of(car1, car2);
-        List<Transport> expectedTruckList = List.of(truck);
-        MixParking expectedMixParking = new MixParking(5, 2);
-        /**expectedMixParking.carList = expectedCarList;
-        expectedMixParking.truckList = expectedTruckList;*/
-
         MixParking mixParking = new MixParking(5, 2);
-        mixParking.add(car1);
-        mixParking.add(car2);
-        mixParking.add(truck);
-
-        assertThat(expectedMixParking).isEqualTo(mixParking);
+        assertThat(mixParking.add(car1)).isTrue();
+        assertThat(mixParking.add(car2)).isTrue();
+        assertThat(mixParking.add(truck)).isTrue();
     }
+
+    @Test
+    void whenTwoCarsAndTruckToCarParking() {
+        Transport car1 = new Car("Lada", "в777аа");
+        Transport car2 = new Car("Mercedes", "к520аа");
+        Transport truck = new Truck("Kamaz", "а111аа", 1);
+        MixParking mixParking = new MixParking(5, 0);
+        assertThat(mixParking.add(car1)).isTrue();
+        assertThat(mixParking.add(car2)).isTrue();
+        assertThat(mixParking.add(truck)).isTrue();
+    }
+
+    @Test
+    void whenOnlyTrucks() {
+        Transport car1 = new Car("Lada", "в777аа");
+        Transport truck1 = new Truck("Kamaz", "а111аа", 1);
+        Transport truck2 = new Truck("Kamaz", "а111аа", 2);
+        MixParking mixParking = new MixParking(0, 2);
+        assertThat(mixParking.add(car1)).isFalse();
+        assertThat(mixParking.add(truck1)).isTrue();
+        assertThat(mixParking.add(truck2)).isTrue();
+    }
+
+    @Test
+    void whenOnlyTrucksToBothParking() {
+        Transport truck1 = new Truck("Kamaz", "а111аа", 1);
+        Transport truck2 = new Truck("Kamaz", "а111аа", 2);
+        MixParking mixParking = new MixParking(2, 1);
+        assertThat(mixParking.add(truck1)).isTrue();
+        assertThat(mixParking.add(truck2)).isTrue();
+    }
+
 }
